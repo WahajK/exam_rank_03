38279fc8390b7e8b990d1dcc1f25a1c64fa95f03
WahajK <wahajkhan788@gmail.com>
2026-01-14 19:37:42 +0100

    Finished nqueens

--- Level_2/nqueens/nqueens.c
+++ Level_2/nqueens/nqueens.c
@@ -5,23 +5,98 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: muhakhan <muhakhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/16 17:25:25 by muhakhan          #+#    #+#             */
-/*   Updated: 2025/12/16 19:29:07 by muhakhan         ###   ########.fr       */
+/*   Created: 2026/01/14 19:11:14 by muhakhan          #+#    #+#             */
+/*   Updated: 2026/01/14 19:14:17 by muhakhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//My practice solution for the subject
-
-#include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <stdlib.h>
 
-int	main(int ac, char *av[])
+static void write_number(int n)
 {
-	int	n;
-	if (ac != 2)
-		return (1);
-	n = atoi(av[1]);
-	if (n < 0)
-	
+    char c;
+
+    if (n >= 10)
+        write_number(n / 10);
+    c = (n % 10) + '0';
+    write(1, &c, 1);
+}
+
+void print_solution(int *pos, int n)
+{
+    int i;
+
+    i = 0;
+    while (i < n)
+    {
+        write_number(pos[i]);
+        if (i + 1 < n)
+            write(1, " ", 1);
+        i++;
+    }
+    write(1, "\n", 1);
+}
+
+int is_safe(int col, int row, int *pos)
+{
+    int i;
+
+    i = 0;
+    while (i < col)
+    {
+        if (pos[i] == row)
+            return (0);
+        if (pos[i] - i == row - col)
+            return (0);
+        if (pos[i] + i == row + col)
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+void backtrack(int col, int n, int *pos)
+{
+    int row;
+
+    if (col == n)
+    {
+        print_solution(pos, n);
+        return;
+    }
+    row = 0;
+    while (row < n)
+    {
+        if (is_safe(col, row, pos))
+        {
+            pos[col] = row;
+            backtrack(col + 1, n, pos);
+        }
+        row++;
+    }
+}
+
+void solve_n_queens(int n)
+{
+    int *pos;
+
+    pos = (int *)malloc(sizeof(int) * n);
+    if (!pos)
+        return;
+    backtrack(0, n, pos);
+    free(pos);
+}
+
+int main(int argc, char **argv)
+{
+    int n;
+
+    if (argc != 2)
+        return (0);
+    n = atoi(argv[1]);
+    if (n <= 0)
+        return (0);
+    solve_n_queens(n);
+    return (0);
 }
